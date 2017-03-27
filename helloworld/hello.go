eae02d8f31407773cd9d1e80a2a8ce4c35257491
junli3 <junli3@iflytek.com>
2017-03-27 14:20:41 +0800

    使用goland测试提交

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -6,5 +6,8 @@ import (
 	"github.com/lj317/stringutil"
 )
 func main() {
+	a,b:="1","2"
+	fmt.Printf("a=" + a + "\n")
+	fmt.Printf("b=" + b + "\n")
 	fmt.Printf(stringutil.Reverse("jl, olleh")+"\n")
 }
--- stringutil/reverse.go
+++ stringutil/reverse.go
@@ -9,3 +9,20 @@ func Reverse(s string) string {
 	}
 	return string(r)
 }
+
+
+func rev(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func Myint() int {
+	return 1
+}
+
+func OtherInt() int {
+	return 2
+}
